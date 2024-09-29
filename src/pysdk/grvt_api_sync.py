e9c0e2f678b541a6fc329841c1f7402b80047288
Aaron Ong <aaronongcs@gmail.com>
2024-09-29 23:43:08 +0800

    [TRADE-99] Use Dacite Decode

--- src/pysdk/grvt_api_sync.py
+++ src/pysdk/grvt_api_sync.py
@@ -5,6 +5,8 @@ from dacite import Config, from_dict
 from . import types
 from .grvt_api_base import GrvtApiConfig, GrvtApiSyncBase, GrvtError
 
+# mypy: disable-error-code="no-any-return"
+
 
 class GrvtApiSync(GrvtApiSyncBase):
     def __init__(self, config: GrvtApiConfig):
@@ -18,7 +20,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/instrument", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiGetInstrumentResponse(**resp)
+        return from_dict(types.ApiGetInstrumentResponse, resp, Config(cast=[Enum]))
 
     def get_all_instruments_v1(
         self, req: types.ApiGetAllInstrumentsRequest
@@ -34,7 +36,9 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/instruments", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiGetFilteredInstrumentsResponse(**resp)
+        return from_dict(
+            types.ApiGetFilteredInstrumentsResponse, resp, Config(cast=[Enum])
+        )
 
     def mini_ticker_v1(
         self, req: types.ApiMiniTickerRequest
@@ -42,7 +46,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/mini", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiMiniTickerResponse(**resp)
+        return from_dict(types.ApiMiniTickerResponse, resp, Config(cast=[Enum]))
 
     def ticker_v1(
         self, req: types.ApiTickerRequest
@@ -50,7 +54,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/ticker", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiTickerResponse(**resp)
+        return from_dict(types.ApiTickerResponse, resp, Config(cast=[Enum]))
 
     def orderbook_levels_v1(
         self, req: types.ApiOrderbookLevelsRequest
@@ -58,7 +62,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/book", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiOrderbookLevelsResponse(**resp)
+        return from_dict(types.ApiOrderbookLevelsResponse, resp, Config(cast=[Enum]))
 
     def public_trades_v1(
         self, req: types.ApiPublicTradesRequest
@@ -66,7 +70,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/trades", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiPublicTradesResponse(**resp)
+        return from_dict(types.ApiPublicTradesResponse, resp, Config(cast=[Enum]))
 
     def public_trade_history_v1(
         self, req: types.ApiPublicTradeHistoryRequest
@@ -74,7 +78,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/trade_history", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiPublicTradeHistoryResponse(**resp)
+        return from_dict(types.ApiPublicTradeHistoryResponse, resp, Config(cast=[Enum]))
 
     def candlestick_v1(
         self, req: types.ApiCandlestickRequest
@@ -82,7 +86,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/kline", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiCandlestickResponse(**resp)
+        return from_dict(types.ApiCandlestickResponse, resp, Config(cast=[Enum]))
 
     def funding_rate_v1(
         self, req: types.ApiFundingRateRequest
@@ -90,7 +94,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/funding", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiFundingRateResponse(**resp)
+        return from_dict(types.ApiFundingRateResponse, resp, Config(cast=[Enum]))
 
     def settlement_price_v1(
         self, req: types.ApiSettlementPriceRequest
@@ -98,7 +102,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(False, self.md_rpc + "/full/v1/settlement", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiSettlementPriceResponse(**resp)
+        return from_dict(types.ApiSettlementPriceResponse, resp, Config(cast=[Enum]))
 
     def create_order_v1(
         self, req: types.ApiCreateOrderRequest
@@ -106,7 +110,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/create_order", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiCreateOrderResponse(**resp)
+        return from_dict(types.ApiCreateOrderResponse, resp, Config(cast=[Enum]))
 
     def cancel_order_v1(
         self, req: types.ApiCancelOrderRequest
@@ -114,7 +118,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/cancel_order", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.AckResponse(**resp)
+        return from_dict(types.AckResponse, resp, Config(cast=[Enum]))
 
     def cancel_all_orders_v1(
         self, req: types.ApiCancelAllOrdersRequest
@@ -122,7 +126,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/cancel_all_orders", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.AckResponse(**resp)
+        return from_dict(types.AckResponse, resp, Config(cast=[Enum]))
 
     def get_order_v1(
         self, req: types.ApiGetOrderRequest
@@ -130,7 +134,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/order", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiGetOrderResponse(**resp)
+        return from_dict(types.ApiGetOrderResponse, resp, Config(cast=[Enum]))
 
     def open_orders_v1(
         self, req: types.ApiOpenOrdersRequest
@@ -138,7 +142,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/open_orders", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiOpenOrdersResponse(**resp)
+        return from_dict(types.ApiOpenOrdersResponse, resp, Config(cast=[Enum]))
 
     def order_history_v1(
         self, req: types.ApiOrderHistoryRequest
@@ -146,7 +150,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/order_history", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiOrderHistoryResponse(**resp)
+        return from_dict(types.ApiOrderHistoryResponse, resp, Config(cast=[Enum]))
 
     def private_trade_history_v1(
         self, req: types.ApiPrivateTradeHistoryRequest
@@ -154,7 +158,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/trade_history", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiPrivateTradeHistoryResponse(**resp)
+        return from_dict(types.ApiPrivateTradeHistoryResponse, resp, Config(cast=[Enum]))
 
     def positions_v1(
         self, req: types.ApiPositionsRequest
@@ -162,13 +166,13 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/positions", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiPositionsResponse(**resp)
+        return from_dict(types.ApiPositionsResponse, resp, Config(cast=[Enum]))
 
     def deposit_v1(self, req: types.ApiDepositRequest) -> types.AckResponse | GrvtError:
         resp = self._post(True, self.td_rpc + "/full/v1/deposit", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.AckResponse(**resp)
+        return from_dict(types.AckResponse, resp, Config(cast=[Enum]))
 
     def deposit_history_v1(
         self, req: types.ApiDepositHistoryRequest
@@ -176,13 +180,13 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/deposit_history", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiDepositHistoryResponse(**resp)
+        return from_dict(types.ApiDepositHistoryResponse, resp, Config(cast=[Enum]))
 
     def transfer_v1(self, req: types.ApiTransferRequest) -> types.AckResponse | GrvtError:
         resp = self._post(True, self.td_rpc + "/full/v1/transfer", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.AckResponse(**resp)
+        return from_dict(types.AckResponse, resp, Config(cast=[Enum]))
 
     def transfer_history_v1(
         self, req: types.ApiTransferHistoryRequest
@@ -190,7 +194,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/transfer_history", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiTransferHistoryResponse(**resp)
+        return from_dict(types.ApiTransferHistoryResponse, resp, Config(cast=[Enum]))
 
     def withdrawal_v1(
         self, req: types.ApiWithdrawalRequest
@@ -198,7 +202,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/withdrawal", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.AckResponse(**resp)
+        return from_dict(types.AckResponse, resp, Config(cast=[Enum]))
 
     def withdrawal_history_v1(
         self, req: types.ApiWithdrawalHistoryRequest
@@ -206,7 +210,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/withdrawal_history", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiWithdrawalHistoryResponse(**resp)
+        return from_dict(types.ApiWithdrawalHistoryResponse, resp, Config(cast=[Enum]))
 
     def sub_account_summary_v1(
         self, req: types.ApiSubAccountSummaryRequest
@@ -214,7 +218,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/account_summary", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiSubAccountSummaryResponse(**resp)
+        return from_dict(types.ApiSubAccountSummaryResponse, resp, Config(cast=[Enum]))
 
     def sub_account_history_v1(
         self, req: types.ApiSubAccountHistoryRequest
@@ -222,7 +226,7 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/account_history", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiSubAccountHistoryResponse(**resp)
+        return from_dict(types.ApiSubAccountHistoryResponse, resp, Config(cast=[Enum]))
 
     def aggregated_account_summary_v1(
         self, req: types.EmptyRequest
@@ -230,7 +234,9 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/aggregated_account_summary", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiAggregatedAccountSummaryResponse(**resp)
+        return from_dict(
+            types.ApiAggregatedAccountSummaryResponse, resp, Config(cast=[Enum])
+        )
 
     def funding_account_summary_v1(
         self, req: types.EmptyRequest
@@ -238,4 +244,6 @@ class GrvtApiSync(GrvtApiSyncBase):
         resp = self._post(True, self.td_rpc + "/full/v1/funding_account_summary", req)
         if resp.get("code"):
             return GrvtError(**resp)
-        return types.ApiFundingAccountSummaryResponse(**resp)
+        return from_dict(
+            types.ApiFundingAccountSummaryResponse, resp, Config(cast=[Enum])
+        )
